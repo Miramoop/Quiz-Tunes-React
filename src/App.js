a40b72bc485c11312b5b8223529adef10512822b
Miranda Morris <mmorris@bizstream.com>
2024-06-13 16:12:38 -0400

    Addition of Quiz Results Page
    Currently only the return to home button works

--- src/App.js
+++ src/App.js
@@ -1,51 +1,95 @@
-import logo from './logo.svg';
-import Header from './components/Header.js'
-import Footer from './components/Footer.js';
-import Home from './components/Home.js'
-import Quiz from './components/Quiz.js';
-import QuizComplete from './components/QuizComplete.js';
-import questionsData from './data/questions.json'
-import weightsData from './data/weights.json'
-import { useState, useEffect } from 'react';
-import './App.css';
-import './styles/styles.css'
+import logo from "./logo.svg";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Home from "./components/Home.js";
+import Quiz from "./components/Quiz.js";
+import QuizComplete from "./components/QuizComplete.js";
+import QuizResults from "./components/QuizResults.js";
+import questionsData from "./data/questions.json";
+import weightsData from "./data/weights.json";
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./styles/styles.css";
 
 function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(-1);
   const [questions, setQuestions] = useState([]);
   const [weights, setWeights] = useState([]);
   const [isQuizComplete, setIsQuizComplete] = useState(false);
+  const [isCalculatedResults, setCalculatedResults] = useState(false);
   const [error, setError] = useState(false);
   useEffect(() => {
-    if (isQuizComplete){
-      console.log('Quiz is Complete');
+    if (isQuizComplete) {
+      console.log("Quiz is Complete");
     }
-  }, [isQuizComplete])
+  }, [isQuizComplete]);
+
+  useEffect(() => {
+    if (isCalculatedResults) {
+      console.log("Calculate the Results");
+    }
+  }, [isCalculatedResults]);
+
   useEffect(() => {
     setError(false);
-    try{
+    try {
       setQuestions(questionsData);
-      setWeights(weightsData); 
-    }
-    catch(e){
+      setWeights(weightsData);
+    } catch (e) {
       console.error(e);
       setError(true);
     }
-  }, [])
-  const handleStartQuiz= () => {
+  }, []);
+
+  const handleStartQuiz = () => {
     setCurrentQuestionIndex(currentQuestionIndex + 1);
-    console.log('Start Quiz');
-  }
+    console.log("Start Quiz");
+  };
+
+  const handleCalculateResults = () => {
+    setCalculatedResults(true);
+    console.log("Calculate the Results!");
+  };
+
+  const resetQuiz = () => {
+    setCurrentQuestionIndex(-1);
+    setIsQuizComplete(false);
+    setCalculatedResults(false);
+    console.log("Reset Quiz");
+  };
+
+  const displaySpotifyLink = () => {
+    console.log("Display Spotify Link");
+  };
+
+  const fetchYouTubeDataAndDisplay = () => {
+    console.log("Fetch YouTube Data & Display");
+  };
 
   return (
     <div className="App">
       <Header />
       {error && <div>I&apos;m an Error</div>}
-      {currentQuestionIndex === -1 && 
-      <Home startQuiz={handleStartQuiz}/>
-      }
-      {currentQuestionIndex === 0 && !isQuizComplete && <Quiz questions={questions} weights={weights} setIsQuizComplete={setIsQuizComplete}/>}
-      {isQuizComplete && <QuizComplete/> }
+      {currentQuestionIndex === -1 && <Home startQuiz={handleStartQuiz} />}
+      {currentQuestionIndex >= 0 && currentQuestionIndex < questions.length && !isQuizComplete && (
+        <Quiz
+          questions={questions}
+          weights={weights}
+          setIsQuizComplete={setIsQuizComplete}
+        />
+      )}
+      {isQuizComplete && !isCalculatedResults &&(
+        <QuizComplete
+          handleCalculateResults={handleCalculateResults}
+        />
+      )}
+      {isCalculatedResults && (
+      <QuizResults 
+        resetQuiz={resetQuiz}
+        displaySpotifyLink={displaySpotifyLink}
+        fetchYouTubeDataAndDisplay={fetchYouTubeDataAndDisplay}
+      />
+      )}
       <Footer />
     </div>
   );
--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from "react";
-const QuizResults = ({}) => (
-    <section id="results">
-    <div id="resultsContent" role="alert"></div>
+const QuizResults = ({
+  resetQuiz,
+  displaySpotifyLink,
+  fetchYouTubeDataAndDisplay,
+}) => (
+  <section id="results">
+    <div id="resultsContent" role="alert">
+      Results are Here!
+    </div>
     <div id="videoContent"></div>
     <div id="spotifyContent"></div>
     <div id="buttonHolder">
-        <ul>
-            <li><button id="resetQuizButton" aria-label="Reset and Start the Quiz Over">Return to HomePage</button></li>
-            <li><button id="spotifyTrackButton" aria-label="View the Spotify Track Link">View the Spotify Track</button></li>
-            <li><button id="youtubeVideoButton" aria-label="View the YouTube Video">View the YouTube Video</button></li>
-        </ul>
+      <ul>
+        <li>
+          <button
+            id="resetQuizButton"
+            onClick={resetQuiz} //Add into App.js, ResetQuiz Function
+            aria-label="Reset and Start the Quiz Over"
+          >
+            Return to HomePage
+          </button>
+        </li>
+        <li>
+          <button
+            id="spotifyTrackButton"
+            onClick={displaySpotifyLink} //Add into App.js, displaySpotifyLink Function
+            aria-label="View the Spotify Track Link"
+          >
+            View the Spotify Track
+          </button>
+        </li>
+        <li>
+          <button
+            id="youtubeVideoButton"
+            onClick={fetchYouTubeDataAndDisplay} //Add into App.js, fetchYouTubeDataAndDisplay Function
+            aria-label="View the YouTube Video"
+          >
+            View the YouTube Video
+          </button>
+        </li>
+      </ul>
     </div>
-    </section>
-  );
+  </section>
+);
 export default QuizResults;
