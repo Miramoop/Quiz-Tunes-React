6885429538af2b045dce669f3a71a5a4c4a3114d
Miranda Morris <mmorris@bizstream.com>
2024-06-14 10:08:13 -0400

    Switch Font Awesome Icons to NPM Packages,

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Home from "./components/Home.js";
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
   <footer className="active">
     <p> Check out my Socials Below: </p>
-    <a href="https://github.com/Miramoop" target="_blank" rel="noopener, noreferrer" aria-label="GitHub Page"><i class="fa-brands fa-github"></i></a>
-    <a href="https://www.linkedin.com/in/mirandamorris845/" target="_blank" rel="noopener, noreferrerr" aria-label="Linkedin Page"><i class="fa-brands fa-linkedin"></i></a>
+    <a
+      href="https://github.com/Miramoop"
+      target="_blank"
+      rel="noopener, noreferrer"
+      aria-label="GitHub Page"
+    >
+      <FontAwesomeIcon icon={faGithub} />
+    </a>
+
+    <a
+      href="https://www.linkedin.com/in/mirandamorris845/"
+      target="_blank"
+      rel="noopener, noreferrerr"
+      aria-label="Linkedin Page"
+    >
+      <FontAwesomeIcon icon={faLinkedin} />
+    </a>
   </footer>
 );
 
