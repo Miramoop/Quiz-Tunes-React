2e91b6be2cf137edfaf7bccccbde6a2352f98514
Miranda Morris <mmorris@bizstream.com>
2024-07-03 14:22:24 -0400

    Update Homepage Button Colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ const Home = ({ startQuiz }) => (
   <section id="home">
     <h2 aria-label="Welcome to Quiz Tunes!">Welcome to Quiz Tunes!</h2>
     <button
+      className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
       id="startQuizButton"
       onClick={startQuiz}
       aria-label="Start the Quiz"
