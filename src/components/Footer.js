4be22417a1bfa4ca4abbcfef65baa8b0580bf486
Miranda Morris <mmorris@bizstream.com>
2024-07-03 13:24:14 -0400

    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
-  <footer className="active">
-    <p> Check out my Socials Below: </p>
-    <a
-      href="https://github.com/Miramoop"
-      target="_blank"
-      rel="noopener, noreferrer"
-      aria-label="GitHub Page"
-    >
-      <FontAwesomeIcon icon={faGithub} />
-    </a>
-
-    <a
-      href="https://www.linkedin.com/in/mirandamorris845/"
-      target="_blank"
-      rel="noopener, noreferrer"
-      aria-label="Linkedin Page"
-    >
-      <FontAwesomeIcon icon={faLinkedin} />
-    </a>
+  <footer>
+    <nav>
+      <h6>Check out my Socials</h6>
+      <div>
+        <a
+          href="https://github.com/Miramoop"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub Page"
+        >
+          <FontAwesomeIcon icon={faGithub} size="1.5x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mirandamorris845/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn Page"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="1.5x" />
+        </a>
+      </div>
+    </nav>
   </footer>
 );
 
 export default Footer;
+
