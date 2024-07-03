b604d5f808853efc67e608614de4fe233f337602
Miranda Morris <mmorris@bizstream.com>
2024-07-03 09:26:09 -0400

    PR #6 Additional Fixes

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import StepsComponent from "./StepsComponent";
 
 const Quiz = ({ questions, weights, updateWeights, setIsQuizComplete }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
