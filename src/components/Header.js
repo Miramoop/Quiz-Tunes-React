5f89801ed7cffbf90aae7e85108b17787f27de69
Miranda Morris <mmorris@bizstream.com>
2024-07-12 11:28:25 -0400

    Fixing Header Styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,94 +4,96 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <a aria-label="Navigation Bar Button">
-              <FontAwesomeIcon icon={faBars} size="2x" />
-            </a>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              {" "}
-              <a
-                href="https://github.com/Miramoop"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="GitHub Page"
-              >
-                GitHub
-              </a>
-            </li>
-            <li>
-              {" "}
-              <a
-                href="https://www.linkedin.com/in/mirandamorris845/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="LinkedIn Page"
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              {" "}
-              <a
-                href="https://www.miramoop.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Portfolio Website"
-              >
-                Portfolio
-              </a>
-            </li>
-          </ul>
+    <header>
+      <div className="navbar">
+        <div className="navbar-start">
+        <img
+            id="headerLogo"
+            src="/img/logo/QuizTunesLogo.png"
+            alt="Quiz Tunes Logo"
+          />
         </div>
-        <p>Quiz Tunes</p>
+        <div className="navbar-end">
+        <div className="dropdown lg:hidden">
+            <button 
+              className="header-navigation-bar-button lg:hidden"
+              aria-label="Navigation Bar Button"
+            >
+              <FontAwesomeIcon icon={faBars} size="1x" />
+            </button>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow right-0 p-2"
+            >
+              <li className="menu-item">
+                <a 
+                  href="https://github.com/Miramoop"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="GitHub Page"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li className="menu-item">
+                <a
+                  href="https://www.linkedin.com/in/mirandamorris845/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="LinkedIn Page"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li className="menu-item">
+                <a
+                  href="https://www.miramoop.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Portfolio Website"
+                >
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li className="menu-item">
+                <a
+                  href="https://github.com/Miramoop"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="GitHub Page"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li className="menu-item">
+                <a
+                  href="https://www.linkedin.com/in/mirandamorris845/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="LinkedIn Page"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li className="menu-item">
+                <a
+                  href="https://www.miramoop.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Portfolio Website"
+                >
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            {" "}
-            <a
-              href="https://github.com/Miramoop"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="GitHub Page"
-            >
-              GitHub
-            </a>
-          </li>
-          <li>
-            {" "}
-            <a
-              href="https://www.linkedin.com/in/mirandamorris845/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="LinkedIn Page"
-            >
-              LinkedIn
-            </a>
-          </li>
-          <li>
-            {" "}
-            <a
-              href="https://www.miramoop.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Portfolio Website"
-            >
-              Portfolio
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end"></div>
-    </div>
+    </header>
   );
 }
 
