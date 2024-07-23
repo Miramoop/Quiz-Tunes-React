cda06f0b6d258a5961db487a4695cdb337850596
Miranda Morris <mmorris@bizstream.com>
2024-07-23 16:38:34 -0400

    PR #20 Fixes: Remove Aside tag & Fix Nav tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,48 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
   <footer className="footer footer-center text-primary-content p-2">
-    <aside>
-      <img
-        id="miramoopLogo"
-        src="/img/logo/miramoopLogo.png"
-        alt="Miramoop Logo"
-      />
-      <p className="font-bold">Miramoop Coding</p>
-      <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-    </aside>
+    <img
+      id="miramoopLogo"
+      src="/img/logo/miramoopLogo.png"
+      alt="Miramoop Logo"
+    />
+    <p className="font-bold">Miramoop Coding</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
     <nav>
-      <div className="grid grid-flow-col gap-4 social-links">
+      <ul className="social-links">
+        <li>
+          <a
+            href="https://github.com/Miramoop"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub Page"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/mirandamorris845/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn Page"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.miramoop.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Portfolio Website"
+          >
+            <FontAwesomeIcon icon={faBriefcase} size="2x" />
+          </a>
+        </li>
+      </ul>
+
+      {/* <div className="grid grid-flow-col gap-4 social-links">
         <a
           href="https://github.com/Miramoop"
           target="_blank"
@@ -40,7 +71,7 @@ const Footer = () => (
         >
           <FontAwesomeIcon icon={faBriefcase} size="2x" />
         </a>
-      </div>
+      </div> */}
     </nav>
   </footer>
 );
