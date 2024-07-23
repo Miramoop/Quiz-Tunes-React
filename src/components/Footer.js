da889163f7242661f72b5157067fa5cf7174b11b
Miranda Morris <mmorris@bizstream.com>
2024-07-23 18:05:54 -0400

    Updates to Header & Footer Styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,47 +4,40 @@ import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
-<footer className="footer footer-center text-primary-content p-2"> 
-  <aside>
-    <img
-      id="miramoopLogo"
-      src="/img/logo/miramoopLogo.png"
-      alt="Miramoop Logo"
-    />
-    <p className="font-bold">
-      Miramoop Coding
-    </p>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-  </aside>
-  <nav>
-    <div className="grid grid-flow-col gap-4 social-links">
-      <a
-        href="https://github.com/Miramoop"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="GitHub Page"
-      >
-        <FontAwesomeIcon icon={faGithub} size="2x" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/mirandamorris845/"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="LinkedIn Page"
-      >
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-      </a>
-      <a
-        href="https://www.miramoop.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Portfolio Website"
-      >
-         <FontAwesomeIcon icon={faBriefcase} size="2x" />
-      </a>
-    </div>
-  </nav>
-</footer>
+  <footer className="footer footer-center text-primary-content">
+    <aside>
+      <p className="companyName">Miramoop Coding</p>
+      <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+      <nav>
+        <div className="grid grid-flow-col social-links">
+          <a
+            href="https://github.com/Miramoop"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub Page"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/mirandamorris845/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn Page"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a
+            href="https://www.miramoop.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Portfolio Website"
+          >
+            <FontAwesomeIcon icon={faBriefcase} size="2x" />
+          </a>
+        </div>
+      </nav>
+    </aside>
+  </footer>
 );
 
 export default Footer;
