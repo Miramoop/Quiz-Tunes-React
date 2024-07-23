22119325e263485b50d000a89b40c9af37a288d7
Miranda Morris <mmorris@bizstream.com>
2024-07-23 18:43:23 -0400

    youtube mockup basic design

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -1,4 +1,5 @@
 import React from "react";
+import YoutubeMockup from "./YoutubeMockup";
 
 const QuizResults = ({
   resetQuiz,
@@ -48,28 +49,7 @@ const QuizResults = ({
       ) : (
         <p>No song information available.</p>
       )}
-
-      {youTubeVideos && youTubeVideos.length > 0 && (
-        <div id="videoSection">
-          {youTubeVideos.map((video) => (
-            <div key={video.id.videoId} className="video-container">
-              <iframe
-                title={video.snippet.title}
-                src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-              <noscript>
-                <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`}>
-                  Your browser does not support this type of embed. Watch the
-                  video here instead.
-                </a>
-              </noscript>
-            </div>
-          ))}
-        </div>
-      )}
+      <YoutubeMockup youTubeVideos={youTubeVideos} />
 
       <div id="buttonHolder">
         <button
--- src/components/YoutubeMockup.js
+++ src/components/YoutubeMockup.js
@@ -1,30 +1,52 @@
 import React from "react";
+import he from "he";
 
 const YoutubeMockup = ({ youTubeVideos }) => {
+  const youtubeVideoUrl =
+    youTubeVideos && youTubeVideos.length > 0
+      ? `https://www.youtube.com/watch?v=${youTubeVideos[0].id.videoId}`
+      : "https://www.youtube.com";
+  const youtubeTitle =
+    youTubeVideos && youTubeVideos.length > 0
+      ? he.decode(youTubeVideos[0].snippet.title)
+      : "No video title available";
   return (
-    <section id="results">
-      {youTubeVideos && youTubeVideos.length > 0 && (
-        <div id="videoSection">
-          {youTubeVideos.map((video) => (
-            <div key={video.id.videoId} className="video-container">
-              <iframe
-                title={video.snippet.title}
-                src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-              <noscript>
-                <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`}>
-                  Your browser does not support this type of embed. Watch the
-                  video here instead.
-                </a>
-              </noscript>
-            </div>
-          ))}
-        </div>
-      )}
-    </section>
+    <div className="mockup-browser bg-base-300 border">
+      <div className="mockup-browser-toolbar">
+        <div className="input">{youtubeVideoUrl}</div>
+      </div>
+      <div className="bg-base-200 flex flex-col items-center px-4 py-16">
+        {youTubeVideos && youTubeVideos.length > 0 ? (
+          <div id="videoSection">
+            {youTubeVideos.map((video) => (
+              <div key={video.id.videoId} className="video-container mb-4">
+                <iframe
+                  title={video.snippet.title}
+                  src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="w-full h-64"
+                ></iframe>
+                <div className="videoInfo">
+                  <p>{youtubeTitle}</p>
+                </div>
+                <noscript>
+                  <a
+                    href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+                  >
+                    Your browser does not support this type of embed. Watch the
+                    video here instead.
+                  </a>
+                </noscript>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No videos available</p>
+        )}
+      </div>
+    </div>
   );
 };
 
