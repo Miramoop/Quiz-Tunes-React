bc8458bb42226405174fc3bb45f05d7d646315a4
Miranda Morris <mmorris@bizstream.com>
2024-07-23 16:15:06 -0400

    PR #20 Fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,25 @@ import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
-  <footer>
+  <footer className="footer footer-center text-primary-content p-2">
+    <aside>
+      <img
+        id="miramoopLogo"
+        src="/img/logo/miramoopLogo.png"
+        alt="Miramoop Logo"
+      />
+      <p className="font-bold">Miramoop Coding</p>
+      <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+    </aside>
     <nav>
-      <h6>Check out my Socials</h6>
-      <div>
+      <div className="grid grid-flow-col gap-4 social-links">
         <a
           href="https://github.com/Miramoop"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="GitHub Page"
         >
-          <FontAwesomeIcon icon={faGithub} size="1.5x" />
+          <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
         <a
           href="https://www.linkedin.com/in/mirandamorris845/"
@@ -22,7 +30,15 @@ const Footer = () => (
           rel="noopener noreferrer"
           aria-label="LinkedIn Page"
         >
-          <FontAwesomeIcon icon={faLinkedin} size="1.5x" />
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a
+          href="https://www.miramoop.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Portfolio Website"
+        >
+          <FontAwesomeIcon icon={faBriefcase} size="2x" />
         </a>
       </div>
     </nav>
@@ -30,4 +46,3 @@ const Footer = () => (
 );
 
 export default Footer;
-
